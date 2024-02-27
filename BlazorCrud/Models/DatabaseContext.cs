47d91af334dae5f82e571c87b087f107f879b336
Towkir Ahmed <Towkir Ahmed@DESKTOP-7MELF51>
2024-02-27 15:56:05 +0600

    Add project files.

---
+++ BlazorCrud/Models/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Person> Person { get; set; }
+
+    }
+}
---
+++ BlazorCrud/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorCrud.Models;
+
+namespace BlazorCrud.Services
+{
+    public interface IPersonService
+    {
+        bool AddUpdate(Person person);
+        bool Delete(int id);
+        Person FindById(int id);
+        List<Person> GetAll();
+
+    }
+}
